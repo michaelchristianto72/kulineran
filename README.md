5c0b297258be6b4f6e9e95d9a825524210807a8b
Michael Christianto <michaelchristianto72@gmail.com>
2022-09-26 23:40:44 +0700

    Vue e-commerce app with API

---
+++ README.md
@@ -0,0 +1,25 @@
+# kulineran
+practice Vue e-commerce app with API (from Wahidev Academy Youtube)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Hero.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hero">
+    <!--Desktop-->
+    <div class="d-none d-md-block">
+      <div class="row mt-5">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong><br />in Your Gadget
+              </h2>
+              <p>Choose your favourite food now!</p>
+              <router-link class="btn btn-lg btn-info" to="/foods">
+                Pesan <b-icon-arrow-right />
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+      </div>
+    </div>
+
+    <!--Mobile-->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-5">
+        <div class="col-md-6 mb-2">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong><br />in Your Gadget
+              </h2>
+              <p>Choose your favourite food now!</p>
+              <button class="btn btn-lg btn-info">
+                Pesan <b-icon-arrow-right />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
